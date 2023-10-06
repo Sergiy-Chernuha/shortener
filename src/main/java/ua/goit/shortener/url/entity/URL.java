3d5e3e84c7271e68826fa971a111ec89390257ec
Yelyzaveta Lykhomanova <44315985+LisaBugaichuk@users.noreply.github.com>
2023-10-06 20:22:13 +0300

    Public (#1)
    * Commit 2: Create entity and DTO

---
+++ src/main/java/ua/goit/shortener/url/entity/URL.java
@@ -0,0 +1,31 @@
+package ua.goit.shortener.url.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "URL")
+@Data
+public class URL {
+    @Id
+    @Column(name = "SHORT_URL")
+    private String shortURL;
+
+    @Column(name = "LONG_URL")
+    private String longURL;
+
+    @Column(name = "CREATE_DATE")
+    private Date createDate;
+
+    @Column(name = "CLICKS_COUNT")
+    private Integer clicks;
+
+    public URL(String shortURL) {
+    }
+
+    public URL() {
+
+    }
+}
---
+++ src/main/java/ua/goit/shortener/user/dto/UserDTO.java
@@ -0,0 +1,9 @@
+package ua.goit.shortener.user.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private Long id;
+    private String username;
+}
