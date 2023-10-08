3dbb064dbea5936bae501ef9edd494a40405a6b7
Sergiy-Chernuha <sergiy.chernuha@gmail.com>
2023-10-08 17:48:28 +0300

    delete same empty line

--- src/main/java/ua/goit/shortener/ShortenerApplication.java
+++ src/main/java/ua/goit/shortener/ShortenerApplication.java
@@ -3,11 +3,10 @@ package ua.goit.shortener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class ShortenerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShortenerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShortenerApplication.class, args);
+    }
 }
