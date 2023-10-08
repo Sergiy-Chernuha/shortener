d32bdb56f1fd2e9715d4166f06b41d8e37fa8278
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-10-08 17:33:49 +0300

    crud-service

--- src/main/java/ua/goit/shortener/url/services/impl/CrudUrlServiceImpl.java
+++ src/main/java/ua/goit/shortener/url/services/impl/CrudUrlServiceImpl.java
@@ -1,37 +1,38 @@
-package ua.goit.shortener.url.services;
+package ua.goit.shortener.url.services.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ua.goit.shortener.url.entity.URL;
 import ua.goit.shortener.url.repositories.URLRepository;
+import ua.goit.shortener.url.services.CrudUrlService;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class ShortenedUrlService {
+public class CrudUrlServiceImpl implements CrudUrlService {
     private final URLRepository urlRepository;
     @Autowired
-    public ShortenedUrlService(URLRepository urlRepository) {
+    public CrudUrlServiceImpl(URLRepository urlRepository) {
         this.urlRepository = urlRepository;
     }
-
+    @Override
     public List<URL> getAllURLs() {
         return urlRepository.findAll();
     }
-
+    @Override
     public Optional<URL> getURLByShortURL(String shortURL) {
         return urlRepository.findById(shortURL);
     }
-
+    @Override
     public URL createURL(URL url) {
         return urlRepository.save(url);
     }
-
+    @Override
     public void deleteURL(String shortURL) {
         urlRepository.deleteById(shortURL);
     }
-
+    @Override
     public URL updateURL(URL url) {
         Optional<URL> existingURL = urlRepository.findById(url.getShortURL());
         if (existingURL.isPresent()) {
