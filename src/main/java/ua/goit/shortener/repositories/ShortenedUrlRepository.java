bd707a39d3fc42a284a0ef4803d6aa266d9bba88
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-10-08 16:36:39 +0300

    crud-service

--- src/main/java/ua/goit/shortener/repositories/ShortenedUrlRepository.java
+++
@@ -1,7 +0,0 @@
-package ua.goit.shortener.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ua.goit.shortener.entities.ShortenedUrl;
-
-public interface ShortenedUrlRepository extends JpaRepository<ShortenedUrl, Long> {
-}
