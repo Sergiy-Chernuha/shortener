b73a52f57b6135ca8cec94a7582e47262209b5a8
Antonio <oz0969991415@gmail.com>
2023-10-08 10:50:35 +0300

    added port 9999

--- src/main/java/ua/goit/shortener/url/entity/URL.java
+++ src/main/java/ua/goit/shortener/url/entity/URL.java
@@ -3,7 +3,7 @@ package ua.goit.shortener.url.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 @Table(name = "URL")
@@ -17,13 +17,11 @@ public class URL {
     private String longURL;
 
     @Column(name = "CREATE_DATE")
-    private LocalDateTime createDate;
+    private Date createDate;
 
     @Column(name = "CLICKS_COUNT")
     private Integer clicks;
 
-    @Column(name = "USER_ID")
-    private Long user_id;
 
     public URL(String shortURL) {
     }
