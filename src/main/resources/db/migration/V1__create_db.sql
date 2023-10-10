cf24eb7896d5ac14212737a51074f176491cba09
YuliyaGoncharova <125027049+YuliyaGoncharova@users.noreply.github.com>
2023-10-10 19:55:10 +0200

    Spring Profiles dev and prod. (#6)
    * Spring Profiles dev and prod.
    readme.md
    
    ---------
    
    Co-authored-by: ulka <ulka@ulka.com>
    Co-authored-by: Sergiy-Chernuha <sergiy.chernuha@gmail.com>

--- src/main/resources/db/migration/V1__create_db.sql
+++ src/main/resources/db/migration/V1__create_db.sql
@@ -1,13 +1,15 @@
-CREATE TABLE IF NOT EXISTS USERS (
-    id IDENTITY PRIMARY KEY,
-    nick_name VARCHAR(255) NOT NULL,
-    password VARCHAR(1024) NOT NULL
+CREATE TABLE IF NOT EXISTS USERS
+(
+    id        bigint generated always as identity PRIMARY KEY,
+    nick_name VARCHAR(255)  NOT NULL,
+    password  VARCHAR(1024) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS URL (
-    short_url VARCHAR(200) PRIMARY KEY,
-    long_url VARCHAR(1024) NOT NULL,
-    create_date TIMESTAMP(8) NOT NULL,
-    clicks_count INTEGER NOT NULL,
-    user_id BIGINT REFERENCES users(id) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS URL
+(
+    short_url    VARCHAR(200) PRIMARY KEY,
+    long_url     VARCHAR(1024) NOT NULL,
+    create_date  TIMESTAMP(8)  NOT NULL,
+    clicks_count INTEGER       NOT NULL,
+    user_id      BIGINT REFERENCES users (id) ON DELETE CASCADE
 );
---
+++ src/test/java/ua/goit/shortener/springProfiles/ProfileProdTests.java
@@ -0,0 +1,34 @@
+package ua.goit.shortener.springProfiles;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.util.Assert;
+
+import java.util.Objects;
+
+@SpringBootTest
+@ActiveProfiles("production")
+class ProfileProdTests {
+
+    @Autowired
+    Environment environment;
+
+    @Test
+    void profileIsProd() {
+        String expected = environment.getProperty("test_prop");
+        String actual = "PROD";
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void profileIsNotProd() {
+        String actual = environment.getProperty("test_prop");
+
+        Assert.isTrue(Objects.equals(actual, "PROD"), "INCORRECT ENVIRONMENT CONFIG");
+    }
+}
