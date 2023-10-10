5b804b0205efbcbfb848bab40c251d328abb7c00
Yelyzaveta Lykhomanova <44315985+Elliisiv@users.noreply.github.com>
2023-10-10 19:50:51 +0300

    Delete src/main/java/ua/goit/shortener/user/repositories directory

--- src/main/java/ua/goit/shortener/user/repositories/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package ua.goit.shortener.user.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ua.goit.shortener.user.entity.User;
-
-@Repository
-public interface UsersRepository extends JpaRepository<User, String> {
-}
