9f32c29bc9e88d2e0393f5cc87db3bbccd268bf3
Yelyzaveta Lykhomanova <44315985+Elliisiv@users.noreply.github.com>
2023-10-10 19:50:59 +0300

    Delete src/main/java/ua/goit/shortener/user/services directory

--- src/main/java/ua/goit/shortener/user/services/UserServices.java
+++
@@ -1,9 +0,0 @@
-package ua.goit.shortener.user.services;
-
-import org.springframework.stereotype.Service;
-import ua.goit.shortener.user.entity.User;
-
-@Service
-public interface UserServices {
-    void saveUser(User user);
-}
