db85fe5e4571c8ec40ad973838d18e522a8558cc
Ira <37289156+dickenz@users.noreply.github.com>
2023-10-10 19:06:23 +0300

    Update URLServiceImpl.java

--- src/main/java/ua/goit/shortener/url/services/impl/URLServiceImpl.java
+++ src/main/java/ua/goit/shortener/url/services/impl/URLServiceImpl.java
@@ -2,12 +2,14 @@ package ua.goit.shortener.url.services.impl;
 
 import org.springframework.stereotype.Service;
 import ua.goit.shortener.url.dto.UrlDTO;
+import ua.goit.shortener.url.entity.URL;
 import ua.goit.shortener.url.services.URLService;
 
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Random;
-import java.net.URL;
 
 @Service
 public class URLServiceImpl implements URLService {
@@ -18,13 +20,26 @@ public class URLServiceImpl implements URLService {
         int randomLength = 6 + new Random().nextInt(3); // довжина від 6 до 8 символів
         String randomString = generateRandomString(randomLength);
 
-        return prefix + randomString;
+        // Створення URL
+        URL url = new URL();
+        url.setShortURL(prefix + randomString);
+        url.setLongURL(originalURL);
+        url.setCreateDate(new Date());
+        url.setClicks(0);
+
+        // Термін дії url
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH, 2); // Встановлення терміну на 2 доби
+        Date expiryDate = calendar.getTime();
+        url.setExpiryDate(expiryDate);
+
+        return url.getShortURL();
     }
 
     @Override
     public boolean isValidURL(String shortURL) {
         try {
-            new URL(shortURL).toURI();
+            new java.net.URL(shortURL).toURI();
             return true;
         } catch (URISyntaxException exception) {
             return false;
