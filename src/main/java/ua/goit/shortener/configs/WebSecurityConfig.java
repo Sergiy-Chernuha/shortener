c5eee41797e758ab9ae5098c3cb241f681649eef
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-10-11 12:21:19 +0300

    security

---
+++ src/main/java/ua/goit/shortener/configs/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package ua.goit.shortener.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    private final DataSource dataSource;
+
+    public WebSecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorise ->
+                        authorise.requestMatchers("/").permitAll()
+                                .anyRequest().authenticated()
+                ).formLogin(form -> form
+                        .loginPage("/registarion")
+                        .permitAll())
+                .logout(LogoutConfigurer::permitAll);
+
+        return http.build();
+    }
+}
