ce8ad506a5de8f8fa5a17e6b4b202f5312736382
Ira <37289156+dickenz@users.noreply.github.com>
2023-10-12 18:14:26 +0300

    Update URLRepository.java

--- src/main/java/ua/goit/shortener/url/repositories/URLRepository.java
+++ src/main/java/ua/goit/shortener/url/repositories/URLRepository.java
@@ -10,4 +10,6 @@ import java.util.List;
 public interface URLRepository extends JpaRepository<URL, String> {
     List<URL> findByShortURLContaining(String shortURL);
 
+    URL findByShortURL(String shortURL);
+
 }
