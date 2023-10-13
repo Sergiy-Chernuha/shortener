1a486bf9138895372ede57dd7dbca267a1fcc85b
ulka <ulka@ulka.com>
2023-10-13 21:02:49 +0200

    Метод isShortUrlUnique() створено. Додана перевірка чи є короткий урл унікальним (виклик isShortUrlUnique() в методі createShortURL() ).

--- src/main/java/ua/goit/shortener/url/services/impl/URLServiceImpl.java
+++ src/main/java/ua/goit/shortener/url/services/impl/URLServiceImpl.java
@@ -9,7 +9,6 @@ import ua.goit.shortener.url.services.URLService;
 
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
-import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Random;
 
@@ -27,20 +26,13 @@ public class URLServiceImpl implements URLService {
     public String createShortURL(String originalURL) {
         // генерація посилання
         String prefix = "https://shorter/t3/";
-        int randomLength = 6 + new Random().nextInt(3); // довжина від 6 до 8 символів
-        String randomString = generateRandomString(randomLength);
 
-        String shortURL =  prefix + randomString;
-
-        // Перевірка, чи такий короткий URL вже існує в базі даних
-        List<URL> existingURLs = urlRepository.findByShortURLContaining(shortURL);
-
-        if (existingURLs.isEmpty()) {
-            // Якщо короткий URL ще не існує, то використовуємо його
-            return shortURL;
-        } else {
-            // Якщо короткий URL вже існує, генеруємо новий
-            return createShortURL(originalURL);
+        while (true) {
+            int randomLength = 6 + new Random().nextInt(3); // довжина від 6 до 8 символів
+            String randomString = generateRandomString(randomLength);
+            String shortURL = prefix + randomString;
+            if (isShortUrlUnique(shortURL))
+                return shortURL;
         }
     }
 
@@ -78,5 +70,11 @@ public class URLServiceImpl implements URLService {
         }
 
         return randomString.toString();
+    }
+
+    //перевірка, чи існує short URL в БД
+    public boolean isShortUrlUnique(String url) {
+        List<URL> existingURLs = urlRepository.findByShortURLContaining(url);
+        return existingURLs.isEmpty();
     }
 }
