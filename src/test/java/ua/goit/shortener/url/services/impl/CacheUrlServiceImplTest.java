f39b99347054c2cef024bbf8a878f7bc8dd0b1ac
Sergiy-Chernuha <sergiy.chernuha@gmail.com>
2023-10-17 17:10:10 +0300

    add empty test

---
+++ src/test/java/ua/goit/shortener/url/services/impl/CacheUrlServiceImplTest.java
@@ -0,0 +1,13 @@
+package ua.goit.shortener.url.services.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CacheUrlServiceImplTest {
+
+    @Test
+    void getOriginalURL() {
+    }
+
+}
