807f063bf911c32642efea22776958ec17b0709b
Sergiy-Chernuha <sergiy.chernuha@gmail.com>
2023-11-04 14:01:31 +0200

    refactoring UrlControllerTest

--- src/test/java/ua/goit/shortener/url/controller/UrlControllerTest.java
+++ src/test/java/ua/goit/shortener/url/controller/UrlControllerTest.java
@@ -1,46 +1,26 @@
 package ua.goit.shortener.url.controller;
 
-import jakarta.validation.constraints.NotNull;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.MockedStatic;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.MockBeans;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 import ua.goit.shortener.url.dto.UrlDTO;
 import ua.goit.shortener.url.entity.URL;
 import ua.goit.shortener.url.services.CrudUrlService;
 import ua.goit.shortener.url.services.URLService;
-import ua.goit.shortener.url.services.impl.URLServiceImpl;
 
-import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.mockStatic;
-import static org.hamcrest.Matchers.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @WebMvcTest(UrlController.class)
@@ -53,6 +33,7 @@ class UrlControllerTest {
     CrudUrlService crudUrlService;
 
     @MockBean
+    @Qualifier("V1")
     URLService urlServiceImpl;
 
     @Test
@@ -63,7 +44,7 @@ class UrlControllerTest {
         URL url2 = new URL();
         url2.setShortURL("short_url2");
 
-        List urls = Arrays.asList(ulr1, url2);
+        List<URL> urls = Arrays.asList(ulr1, url2);
 
         when(crudUrlService.getAllURLs()).thenReturn(urls);
         when(urlServiceImpl.mapToDTO(any(URL.class))).then(value -> {
@@ -78,8 +59,7 @@ class UrlControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].shortURL").value("short_url1"))
-                .andExpect(jsonPath("$[1].shortURL").value("short_url2"))
-                ;
+                .andExpect(jsonPath("$[1].shortURL").value("short_url2"));
     }
 
     @Test
@@ -90,7 +70,7 @@ class UrlControllerTest {
         URL url2 = new URL();
         url2.setShortURL("isNotActive");
 
-        List urls = Arrays.asList(ulr1, url2);
+        List<URL> urls = Arrays.asList(ulr1, url2);
 
         when(crudUrlService.getAllURLs()).thenReturn(urls);
         when(urlServiceImpl.isActiveShortURL(any(URL.class))).then(invocation -> {
