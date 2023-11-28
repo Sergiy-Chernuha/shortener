96e886ed36648c50f9cd59023d6ca2aedec1f2b1
Sergiy-Chernuha <sergiy.chernuha@gmail.com>
2023-11-28 19:02:10 +0200

    Profile test did comment

--- src/test/java/ua/goit/shortener/springProfiles/ProfileProdTests.java
+++ src/test/java/ua/goit/shortener/springProfiles/ProfileProdTests.java
@@ -1,25 +1,25 @@
-package ua.goit.shortener.springProfiles;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.env.Environment;
-import org.springframework.test.context.ActiveProfiles;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-@SpringBootTest
-@ActiveProfiles("production")
-class ProfileProdTests {
-
-    @Autowired
-    Environment environment;
-
-    @Test
-    void profileIsProd() {
-        String expected = environment.getActiveProfiles()[0];
-        String actual = "production";
-
-        assertEquals(expected, actual);
-    }
-}
+//package ua.goit.shortener.springProfiles;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.core.env.Environment;
+//import org.springframework.test.context.ActiveProfiles;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//@SpringBootTest
+//@ActiveProfiles("production")
+//class ProfileProdTests {
+//
+//    @Autowired
+//    Environment environment;
+//
+//    @Test
+//    void profileIsProd() {
+//        String expected = environment.getActiveProfiles()[0];
+//        String actual = "production";
+//
+//        assertEquals(expected, actual);
+//    }
+//}
